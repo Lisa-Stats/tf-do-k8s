41506df0e8f3873cf2edd361980bf6bd3bf276da
Lisa-Stats <lisa.stathopulos@gmail.com>
2022-11-29 16:23:49 -0500

    Add db module + db_firewall resource

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "digitalocean" {
   token = var.do_token
 }
 
-resource "digitalocean_vpc" "lisa_vpc" {
+resource "digitalocean_vpc" "my_vpc" {
   name     = "clj-network"
   region   = "tor1"
   ip_range = "10.10.10.0/24"
@@ -16,7 +16,7 @@ module "kubernetes" {
   auto_upgrade       = true
   region             = "tor1"
   kubernetes_version = "1.24.4-do.0"
-  vpc_uuid           = digitalocean_vpc.lisa_vpc.id
+  vpc_uuid           = digitalocean_vpc.my_vpc.id
 
   size        = "s-1vcpu-2gb"
   auto_scale  = false
@@ -40,13 +40,38 @@ module "kubernetes" {
       max_nodes   = 4
       node_count  = 2
       node_labels = {
-        env = "test"
+        env     = "test"
         service = "kubernetes"
         made-by = "terraform"
       }
-      node_tags = ["test-node-pool-tag"]
-      maintenance_policy_day = "sunday"
+      node_tags                     = ["test-node-pool-tag"]
+      maintenance_policy_day        = "sunday"
       maintenance_policy_start_time = "04:00"
     }
+  }
+}
+
+module "db" {
+  source  = "mohsenSy/db/digitalocean"
+  version = "0.3.3"
+
+  name                 = "pg-cluster"
+  private_network_uuid = digitalocean_vpc.my_vpc.id
+  engine               = "pg"
+  size                 = "db-s-1vcpu-1gb"
+  region               = "tor1"
+  node_count           = 1
+  db_version           = "11"
+
+  users                = ["admin"]
+  databases            = ["clj"]
+}
+
+resource "digitalocean_database_firewall" "db_firewall" {
+  cluster_id = module.db.id
+
+  rule {
+    type  = "k8s"
+    value = module.kubernetes.cluster_id
   }
 }
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,24 @@
 output "k8s-cluster-id" {
   value = module.kubernetes.cluster_id
 }
+
+output "host" {
+  value = module.db.host
+}
+
+output "passwords" {
+  sensitive = true
+  value     = module.db.passwords
+}
+
+output "database" {
+  value = module.db.database
+}
+
+output "user" {
+  value = module.db.user
+}
+
+output "cluster_vpc" {
+  value = module.db.private_network_uuid
+}
