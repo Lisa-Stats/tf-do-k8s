4bb19f452942dae07cdb7323053ddc7d1223b775
Lisa-Stats <lisa.stathopulos@gmail.com>
2023-01-19 14:42:15 -0500

    Add remote state backend

--- main.tf
+++ main.tf
@@ -40,8 +40,17 @@ module "kubernetes" {
         made-by = "terraform"
       }
       node_tags                     = ["test-node-pool-tag"]
-      maintenance_policy_day        = "sunday"
-      maintenance_policy_start_time = "04:00"
     }
+  }
+}
+
+terraform {
+  backend "s3" {
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    endpoint                    = "nyc3.digitaloceanspaces.com"
+    region                      = "us-east-1"
+    bucket                      = "tf-k8s-bucket"
+    key                         = "terraform.tfstate"
   }
 }
